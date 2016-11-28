609caf63d736c0763fd6c1243161f0edf418ed3e
josephprasanth <prasanthjoseph_77@yahoo.com>
2016-11-28 01:10:32 +0100

    Did part of the section where I tried to login with the email

---
+++ Registration/activate.php
@@ -0,0 +1,20 @@
+<?php
+
+
+include('includes/config.php');
+include('includes/db.php');
+
+
+if(isset ($_GET['token'])){
+$token = $_GET['token'];
+$query = "update users set status='1' where token= '$token'";
+if($db->query($query)) {
+header("Location:index.php?success=Accoun Activated !!");
+exit();
+
+}
+
+
+}
+
+ ?>
---
+++ Registration/send.php
@@ -0,0 +1,9 @@
+<?php
+
+
+mail('prasanth.joseph@student.kyh.se', 'Mail Subject', 'Message', 'From: prasanth.joseph@student.kyh.se');
+
+
+
+
+ ?>
