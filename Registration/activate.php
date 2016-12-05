7aceef098e4a1b1e83c30be79f8fb294fc9e9ad1
josephprasanth <prasanthjoseph_77@yahoo.com>
2016-12-05 13:06:52 +0100

    Trying to do a reset

--- Registration/activate.php
+++ Registration/activate.php
@@ -9,7 +9,7 @@ if(isset ($_GET['token'])){
 $token = $_GET['token'];
 $query = "update users set status='1' where token= '$token'";
 if($db->query($query)) {
-header("Location:index.php?success=Accoun Activated !!");
+header("Location:index.php?success=Account Activated !!");
 exit();
 
 }
