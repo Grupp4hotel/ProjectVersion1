6d71f16cd14ab6f8a3dede630a832cf0c5af8373
AgnetaA <agnetaaxelsson@hotmail.se>
2016-12-07 20:01:58 +0100

    php-ifierat

---
+++ adminAgneta/bokning.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Glada Geten | Boka</title>
+	<meta charset="utf-8" />
+	<link href="https://fonts.googleapis.com/css?family=Days+One|Russo+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Prociono" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="css/load.css">
+	<link rel="stylesheet" type="text/css" href="css/form.css">
+	<link rel="stylesheet" type="text/css" href="css/button.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+	<script src="js/load.js"></script>
+
+</head>
+
+
+<body class="body">
+<div class="se-pre-con"></div>
+	<header class="mainHeader">
+	<center><a class="logo" href="index.php" <h1>GLADA GETEN</h1></a>
+	<h2>BED & BREAKFAST<h2></center>
+	<br>
+		<nav>
+			<ul>
+				<li><a href="index.php">HEM</a></li>
+				<li class="active"><a href="bokning.php">BOKA</a></li>
+				<li><a href="aktiviteter.php">AKTIVITETER</a></li>
+				<li><a href="galleri.php">GALLERI</a></li>
+				<li><a href="omoss.php">OM GLADA GETEN</a></li>
+				<li><a href="kontakt.php">KONTAKT</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div class="mainContent">
+	<br>
+	<h4><span>Boka</span></h4>
+		<div class="content">
+
+				<article class="topcontent" style="float:left">
+
+					<h2 style="text-align:left">Information</h2>
+					<br>
+					<content style="text-align:left; font-size:1.1em; ">
+					<h2>Vid bokning av rum får du med följande</h2>
+					<p>&#x2714; Härliga sänglinnen i percale, som utlovar en härlig och sval natts sömn.</p>
+					<p>&#x2714; Vår alldeles egna handgjorda tvål att använda i badkaret eller duschen!</p>
+					<p>&#x2714; Tillgång till gemensam bastu</p>
+					<p>&#x2714; Gratis Wifi</p>
+					<p>&#x2714; Minibar</p>
+					<p>&#x2714; Vattenkokare</p>
+					<p>&#x2714; Badlakan</p>
+					</content>
+
+				</article>
+ 
+				<article class="bottomcontent" >
+
+
+					<h2>Bokningsuppgifter</h2>
+					<br>
+					<content >
+						<form id="f0rm">
+							<label></label><br><input class="input" type="text" placeholder="Förnamn"><br>
+							<label></label><br><input class="input" type="text" placeholder="Efternamn"><br>
+							<label></label><br><input class="input" type="date" placeholder="Incheckning" id="inDatum"><br>
+							<label></label><br><input class="input" type="date" placeholder="Utcheckning" id="utDatum"><br>
+							<label>Personer</label><br>
+							<select class="s3lect" id="numPpl">
+							<option class="opti0n" value="blank"></option>
+							<option class="opti0n" value="1">1</option>
+							<option class="opti0n" value="2">2</option>
+							<option class="opti0n" value="3">3</option></select><br>
+							<label for="comments">Övriga önskemål</label><br>
+							<textarea name="comments" class="textarea"></textarea><br>
+							<button class="knapp" type="submit" value="Submit">Boka</button><br>
+						</form>
+					</content>
+
+				</article>
+		</div>
+
+	</div>
+
+	<footer class="mainFooter">
+		<h4></h4>
+		<br>
+		<div class="besök">
+		<h2>Besök oss på plats</h2>
+
+		<p>Glada getenvägen 40</p>
+		<p>111 22 Stockholm</p>
+		</div>
+
+		<div class="kontakt">
+		<h2>Kontakta oss</h2>
+
+		<p>kontakt@g4hotell.se</p>
+		<p>08-111 222 333</p>
+		</div>
+		<div class="karta">
+			<div class="basicBox">
+  							<p>Hitta hit</p>
+  							<a href="kontakt.php"> <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
+    						<rect x='0' y='0' fill='none' width='130' height='65'/>
+  							</svg></a>
+						</div>
+		</div>
+	</footer>
+
+
+
+<script>     //tar datum från första sidan
+
+        överför();
+
+        function överför() {
+
+                var bokningsStart = localStorage.getItem("snabbsök");
+                var hämtadData = JSON.parse(bokningsStart);
+
+                //console.log(hämtadData);
+
+            //i bokning
+            var inDatum = document.getElementById("inDatum");
+            var utDatum = document.getElementById("utDatum");
+            var numPpl = document.getElementById("numPpl");
+
+            inDatum.value = hämtadData[0];
+            utDatum.value = hämtadData[1];
+            numPpl.value = hämtadData[2];
+
+        }
+    </script>
+
+
+</body>
+</html>
---
+++ adminAgneta/galleri.php
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Glada Geten | Galleri</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+	<script src="js/load.js"></script>
+
+	<!--Fancybox-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<link rel="stylesheet" 		   href="js/fancyBox/source/jquery.fancybox.css" type="text/css" media="screen" />
+	<script type="text/javascript" src="js/fancyBox/source/jquery.fancybox.pack.js"></script>
+	<!-- Done-->
+	<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Prociono" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+
+	<link href="https://fonts.googleapis.com/css?family=Days+One|Russo+One" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="css/load.css">
+	<link rel="stylesheet" type="text/css" href="css/button.css">
+	
+</head>
+
+<body class="body">
+<div class="se-pre-con"></div>
+	<header class="mainHeader">
+	<center><a class="logo" href="index.php" <h1>GLADA GETEN</h1></a>
+	<h2>BED & BREAKFAST<h2></center>
+	<br>
+		<nav>
+			<ul>
+				<li><a href="index.php">HEM</a></li>
+				<li><a href="bokning.php">BOKA</a></li>
+				<li><a href="aktiviteter.php">AKTIVITETER</a></li>
+				<li class="active"><a href="galleri.php">GALLERI</a></li>
+				<li><a href="omoss.php">OM GLADA GETEN</a></li>
+				<li><a href="kontakt.php">KONTAKT</a></li>
+			</ul>
+		</nav>
+	</header>
+		
+	<!-- Galleri -->
+	<br>
+	<h4><span>Galleri</span></h4>
+	<br>
+	<style>
+		.gal{
+		padding-top:5%;
+		padding-left:3%;
+		
+	}
+		.gal img{
+	
+			width:24%;
+		}
+			@media only screen and (min-width : 150px) and (max-width : 780px){
+				.gal{width:100%; }
+				.gal img{width:22%; }
+			}
+
+		
+	</style>
+		<div class="gal">
+			<a class="fancybox" rel="group" href="img/18.jpg"><img src="img/18.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/2.jpg"><img src="img/2.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/3.jpg"><img src="img/3.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/4.jpg"><img src="img/4.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/5.jpg"><img src="img/5.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/6.jpg"><img src="img/6.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/20.jpg"><img src="img/20.jpg" alt="" /></a>
+			<a class="fancybox" rel="group" href="img/23.jpg"><img src="img/23.jpg" alt="" /></a>
+
+				<script>
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+</script>
+		</div>
+		<!-- Galleri slut -->
+			
+				
+	
+	
+	<footer class="mainFooter">
+		<h4></h4>
+		<br>
+		<div class="besök">
+		<h2>Besök oss på plats</h2>
+		
+		<p>Glada getenvägen 40</p>
+		<p>111 22 Stockholm</p>
+		</div>
+
+		<div class="kontakt">
+		<h2>Kontakta oss</h2>
+		
+		<p>kontakt@g4hotell.se</p>
+		<p>08-111 222 333</p>
+		</div>
+		<div class="karta">
+			<div class="basicBox">
+  							<p>Hitta hit</p>
+  							<a href="kontakt.php"> <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
+    						<rect x='0' y='0' fill='none' width='130' height='65'/>
+  							</svg></a>
+						</div>
+		</div>
+	</footer>
+
+</body>
+</html>
